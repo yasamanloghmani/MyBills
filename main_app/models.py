152c6d13aa0a64fbe8c720588e3d515f71287554
Yasaman Loghmani <yasamanloghmani@Yasamans-MacBook-Pro.local>
2020-06-03 12:46:05 -0400

    Add Bill Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.models import fields
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Bill(models.Model):
+    EXPENSE = 'exp'
+    INCOME = 'inc'
+    CATAGORY_CHOICES = (
+        (EXPENSE, 'expense'),
+        (INCOME, 'income')
+    )
+    title = models.CharField(max_length=100)
+    catagory = models.CharField(max_length=3, choices=CATAGORY_CHOICES)
+    amount = models.IntegerField()
+    created = models.DateTimeField(default=timezone.now, editable=False)
+    modified = fields.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
