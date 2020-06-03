b839df2fd593f6cabdd924e4c62c9af9fc4d0eeb
Yasaman Loghmani <yasamanloghmani@Yasamans-MacBook-Pro.local>
2020-06-03 11:54:20 -0400

    Add Urls, Templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def index(request):
+  return render(request, 'index.html')
